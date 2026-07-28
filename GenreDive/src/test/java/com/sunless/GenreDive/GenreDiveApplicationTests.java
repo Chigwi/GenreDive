d524c3c2eab503d392268e5600b54e1881c0b68e
allie <allie@Mariana>
2026-07-28 18:17:16 -0500

    initial commit bringing the base on which to build the rest of the project

---
+++ GenreDive/src/test/java/com/sunless/GenreDive/GenreDiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunless.GenreDive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenreDiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
